cfff4bc4863e6e2068aaf02ed2b4bbb43e79d85e
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-10-15 15:03:34 +1300

    completed Stage 4 requirements

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
+
 document.addEventListener('keydown', (e) =>{
     // console.log(e.key.toUpperCase());
     const validKeys = ['A', 'S', 'D', 'F', 'G', 'H', 'J'];
     let pressedKey = e.key.toUpperCase();
     if(validKeys.includes(pressedKey)){
-        console.log(`The '${pressedKey}' key is pressed.`);
+        switch(pressedKey){
+            case "A":
+                let A = new Audio("media/A.mp3");
+                A.play()
+                break;
+            case "S":
+                let S = new Audio("media/S.mp3");
+                S.play()
+                break;
+            case "D":
+                let D = new Audio("media/D.mp3");
+                D.play()
+                break;
+            case "F":
+                let F = new Audio("media/F.mp3");
+                F.play()
+                break;
+            case "G":
+                let G = new Audio("media/G.mp3");
+                G.play()
+                break;
+            case "H":
+                let H = new Audio("media/H.mp3");
+                H.play()
+                break;
+            case "J":
+                let J = new Audio("media/J.mp3");
+                J.play()
+                break;
+        }
+
     }
     else{
         console.warn('This key is unbound');
     }
 })
+
